7dc9315f6cf2ba8843b6de41e5067336dd968576
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-26 16:22:27 +0530

    Create positive.java

---
+++ akhila/positive.java
@@ -0,0 +1,19 @@
+class positive
+{
+public static void main(String args[])
+{
+int num;
+Scanner s=new Scanner(System.in);
+num=s.nextInt();
+if(num>0)
+{
+System.out.println(" Positive");
+
+}
+else if(num<0)
+System.out.println(" Negative");
+else{
+System.out.println(" zero");	
+}
+}
+}
