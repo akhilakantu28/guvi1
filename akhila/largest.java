4153a5f59554ad1436cee5a64e9c9129b5e8161a
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-27 10:18:46 +0530

    Create largest.java

---
+++ akhila/largest.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a, b,c,d;
+		Scanner s =new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		d=c>(a>b?a:b)?c:(a>b?a:b);
+		
+		System.out.println(d);
+		
+		
+		
+		
+		
+		
+	}
+}
