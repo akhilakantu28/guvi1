4e91eb3153ffb1200e94b8a2e83f7e030e7a2253
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-27 12:10:47 +0530

    Create count.java

---
+++ akhila/count.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,count=0;
+		
+		Scanner s =new Scanner(System.in);
+		n=s.nextInt();
+		
+		while(n>0)
+		{
+			n=n/10;
+			count++;
+			
+		}
+		
+	System.out.println(count);	  
+		 
+	}
+}
