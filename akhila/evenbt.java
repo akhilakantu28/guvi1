ed533bb30efeb4d746fc50ed9bc0151305e16d54
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-30 19:11:06 +0530

    Update evenbt.java

--- akhila/evenbt.java
+++ akhila/evenbt.java
@@ -14,10 +14,22 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		k=s.nextInt();
+		int m=0;
 		for(int i=n+1;i<k;i++)
 		{
 			if((i%2)==0)
-			System.out.print(i+ " ");
+			{
+			  if(m<1)
+			   {
+			   System.out.print(i);
+			   }
+			   else
+			   {
+				    System.out.print(" ");
+				    System.out.print(i);
+			   }
+				   
+			}m++;	   
 			
 		}
 		
