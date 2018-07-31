d030052ddd76110cbc306600677431be536d6aa9
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-31 15:50:12 +0530

    Update fib.java

--- akhila/fib.java
+++ akhila/fib.java
@@ -14,7 +14,7 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
          int[] a = new int[n];
-         a[0] = 0;
+         a[0] = 1;
          a[1] = 1;
          for(int i=2;i<n;i++)
          {
