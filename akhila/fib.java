fbb282f7c2f28475b99f5087419b7167cee671d6
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-07-31 15:41:15 +0530

    Create fib.java

---
+++ akhila/fib.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+         int[] a = new int[n];
+         a[0] = 0;
+         a[1] = 1;
+         for(int i=2;i<n;i++)
+         {
+         	a[i]=a[i-1]+a[i-2];
+         }
+         
+ 
+         for(int i=0; i<n; i++){
+                 System.out.print(a[i] + " ");
+         }
+	}
+	
+}
