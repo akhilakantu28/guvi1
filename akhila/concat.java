692885561e7a4f44a36d2411f8cb540b4bd4b03f
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-01 16:21:11 +0530

    Create concat.java

---
+++ akhila/concat.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+	               int n;
+		 String str;
+		 str=sc.next();
+	             String str1;
+	             str1=sc.next();
+        
+                 String str2=str+str1;
+                  	
+                  
+                  System.out.println(str2);
+                  
+          }
+   
+  
+ 
+	
+}
