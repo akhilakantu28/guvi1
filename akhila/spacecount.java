efd3f1c5fefdf675b4ae6cbbe65b03b568b0b6df
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-01 14:18:09 +0530

    Create spacecount.java

---
+++ akhila/spacecount.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int word=1;
+        String str;
+        str=sc.nextLine();
+  for(int i=0;i<str.length();++i)
+  {
+   if(str.charAt(i)==' ')
+    word++;
+  }
+ 
+  System.out.println((word-1));
+ 
+	}
+}
