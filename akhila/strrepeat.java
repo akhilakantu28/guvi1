5430ce9b0b22f6f4888bb60acda89e6a4aaaa950
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-01 16:10:11 +0530

    Create strrepeat.java

---
+++ akhila/strrepeat.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+	               int n;
+		 String str;
+		 str=sc.next();
+		 n=sc.nextInt();
+        
+                  for(int i=1;i<=n;i++)
+                  {
+                  	
+                  
+                  System.out.println(str);
+                  }
+          }
+   
+  
+ 
+	
+}
