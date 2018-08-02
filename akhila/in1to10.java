9892d080aaf86b8d86674aa127d306a914b639c1
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-02 15:22:16 +0530

    Create in1to10.java

---
+++ akhila/in1to10.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	              
+	               int a[]={1,2,3,4,5,6,7,8,9,10};
+		Scanner s=new Scanner(System.in);
+		int n;
+		n=s.nextInt();
+	            if(n==1|n==2|n==3|n==4|n==5|n==6|n==7|n==8|n==9|n==10)
+		System.out.println("yes");
+		else
+		{
+			System.out.println("no");
+			
+		}
+		
+	}
+}
