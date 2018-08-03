21c20c4f490f8c78a3dd80224827d67a80933a41
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-03 13:59:19 +0530

    Create alphnum.java

---
+++ akhila/alphnum.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		switch(n)
+		{
+			case 1:System.out.print("One");
+			        break;
+			 case 2:System.out.print("Two");
+			        break;
+			 case 3:System.out.print("Three");
+			        break;
+			case 4:System.out.print("Four");
+			        break;
+			case 5:System.out.print("Five");
+			        break;
+			case 6:System.out.print("Six");
+			        break;
+			case 7:System.out.print("Seven");
+			        break;
+			case 8:System.out.print("Eight");
+			        break;
+			case 9:System.out.print("Nine");
+			        break;
+			case 10:System.out.print("Ten");
+			        break;
+			
+			
+			       
+		}
+	}
+}
