4ecdafb96bcecf31b42a6618bca8f43ebce899a3
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-04 14:33:59 +0530

    Create six.java

---
+++ set7/six.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+                  	String str;
+                  	str=s.nextLine();
+                 
+        if(System.out.printf(str) == null) {}
+        
+	}
+}
