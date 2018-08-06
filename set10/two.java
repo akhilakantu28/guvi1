3c21ccaa05383ba700cd61c4b58b99436a38b50c
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-06 14:48:55 +0530

    Create two.java

---
+++ set10/two.java
@@ -0,0 +1,29 @@
+/* package whatever; // dont place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	        int n;
+	        Scanner sc=new Scanner(System.in);
+	        int a[]=new int[20];
+	         n=sc.nextInt();
+	       int sum=0;
+	      for(int i=0;i<n;i++)
+	         {
+	         	a[i]=sc.nextInt();
+	         }
+	        
+	         for(int i=0;i<n;i++)
+	         {
+	         	sum=sum+a[i];
+	         }
+	System.out.print(sum);	
+	}
+}
