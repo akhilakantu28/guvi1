3f6c287f976bbbdc4bfd5ae7e748913cda9134d6
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-06 13:40:19 +0530

    Create one.java

---
+++ set7/one.java
@@ -0,0 +1,30 @@
+/* package whatever; // dont place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	              int k;
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.next();
+		k=s.nextInt();
+		for(int i=0;i<k;i++)
+		{
+			System.out.print(str.charAt(i));
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
