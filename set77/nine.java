de5a90c3eeec76bef76acc5d8c601ab018d81262
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 18:57:42 +0530

    Create nine.java

---
+++ set77/nine.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+                  	int n,k;
+                  	
+                       n=s.nextInt();
+                       k=s.nextInt();
+                       	if(n>k)
+                       	{
+                       		int res=n-k;
+                       		if(res%2==0)
+                       	            System.out.println("even");
+                       	            else
+                       	            System.out.println("odd");
+                       	}
+                       
+                 
+                       else
+                       {
+                       	            int res=k-n;
+                       		if(res%2==0)
+                       	            System.out.println("even");
+                       	            else
+                       	            System.out.println("odd");
+                       }
+        
+	}
+}
