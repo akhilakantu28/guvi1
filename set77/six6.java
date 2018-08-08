0d42b9d08c539a557c53d5d00e8090dd95ba03b7
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 19:14:43 +0530

    Create six6.java

---
+++ set77/six6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	        int A,B,C;
+	        int sum=0;
+	        Scanner sc=new Scanner(System.in);
+	           A=sc.nextInt();
+	           B=sc.nextInt();
+	           C=sc.nextInt();
+	           for(int i=1;i<=C;i++)
+	           {
+	           	sum=sum+A;
+	           A=A+B;
+	           }
+	           System.out.print(sum);
+	       
+	      
+	
+	}
+}
