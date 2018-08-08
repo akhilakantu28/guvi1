b4adcb8312337218410df28abbb53bf154be60f5
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 19:30:29 +0530

    Create nine.java

---
+++ set10/nine.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b, c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		int res=(a*b)%c;
+		System.out.print(res);
+	}
+}
