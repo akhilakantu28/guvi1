73d0aa43a7a92d44b8c7d5261be6ffee97d7277b
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 14:09:05 +0530

    Create three.java

---
+++ set1010/three.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	        int celsius,kelvin;
+	        Scanner sc=new Scanner(System.in);
+	        
+	         celsius=sc.nextInt();
+	         kelvin=celsius+273;
+	       
+	      
+	System.out.print(kelvin);	
+	}
+}
