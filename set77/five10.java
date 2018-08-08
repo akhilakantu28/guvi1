5cfd1cac1c03e8182e35f5d860373a442ae39147
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 19:12:10 +0530

    Create five10.java

---
+++ set77/five10.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	        int p,t,r;
+	        Scanner sc=new Scanner(System.in);
+	           p=sc.nextInt();
+	           t=sc.nextInt();
+	           r=sc.nextInt();
+	           int res=p*t*r;
+	           int res1=res/100;
+	           System.out.print(res1);
+	       
+	      
+	
+	}
+}
