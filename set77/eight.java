fcdb98c628f732523e951a2b33d36ae9fc7b6baf
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-08 18:55:55 +0530

    Create eight.java

---
+++ set77/eight.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+                  	int n;
+                  	
+                       n=s.nextInt();
+                       
+                       	if(n%7==0)
+                       	{
+                       	System.out.println("yes");
+                       	}
+                       
+                 
+                       else
+                       {	System.out.println("no");
+                       	
+                       }
+        
+	}
+}
