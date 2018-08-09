2817abe78e7c03936bd555c07f89dde7d4eed86b
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-09 12:21:32 +0530

    Create sixsix.java

---
+++ set11/sixsix.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k;
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[20];
+		n=sc.nextInt();
+		k=sc.nextInt();
+		int count=0;
+		for(int i=1;i<=n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=1;i<=n;i++)
+		{
+			if(a[i]%2!=0)
+			{
+				count++;
+			}
+			
+		
+	      	    if(count==k)
+		      {
+			System.out.print(a[i]);
+	             	}
+		}
+	
+	}
+}
