8e3a240445dd4db2cdeccae9d3af2d79cceb3b98
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-10 11:52:38 +0530

    Create ten.java

---
+++ set10/ten.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		int rem=0;
+		int product=1;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		while(n!=0)
+		{
+			rem=n%10;
+			n=n/10;
+			product=product*rem;
+		}
+	System.out.print(product);
+	}
+}
