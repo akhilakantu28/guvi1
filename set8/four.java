b2b1799a423be87275ab4ebd1db4e7a6715a5134
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-10 12:01:02 +0530

    Create four.java

---
+++ set8/four.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		float n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextFloat();
+		int m=(int)n;
+		
+		System.out.print(m+1);
+	}
+}
