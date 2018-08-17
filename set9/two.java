1c5f65e7abe23b8e576a3ab40bb98fdce7b31c93
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-17 12:36:55 +0530

    Create two.java

---
+++ set9/two.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		double a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a*b;
+		System.out.printf("%4.5f",c);
+		
+		
+		
+		
+	}
+}
