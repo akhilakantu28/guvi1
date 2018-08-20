bd26c345726a6b63279f6ec6e95ee8e68e2516e5
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-20 11:14:16 +0530

    Create four.java

---
+++ player/set1/four.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner sc=new Scanner(System.in);
+		str=sc.next();
+		
+		System.out.print(str+".");
+	}
+}
