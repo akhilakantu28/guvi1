f18ef2b5e996b6429c51c764114caa7de55b8de7
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-20 10:21:00 +0530

    Rename 62.java to set62.java

---
+++ set62.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2,n3,n4,n5,n6,n7,n8;
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		n3=s.nextInt();
+		n4=s.nextInt();
+		n5=s.nextInt();
+		n6=s.nextInt();
+		n7=s.nextInt();
+		n8=s.nextInt();
+	   if(n1==n2&&n5==n6&&n3==n8&&n4==n7)
+	   {
+	   	System.out.print("yes");
+	   	
+	   }
+		
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
