d57eb43ac1ff0c06b3f190711e093e6c9c956b5f
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-08-24 12:16:36 +0530

    Create two.java

---
+++ player/set4/two.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k;
+		int a[]=new int[20];
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		k=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+		     if(a[i]==k)
+		     {
+			    count++;
+		     }
+	       	else
+		    {
+		     	
+		    }
+		}
+		if(count==1)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+		System.out.print("no");	
+		}
+	}
+}
