72f0a5701e826b7bac0a88b2f2581f9e19477871
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-07 15:43:57 +0530

    Create secondsmallest.java

---
+++ akhila1/secondsmallest.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a[]=new int[20];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp=a[j];
+					a[j]=a[i];
+					a[i]=temp;
+				}
+			}
+		
+		}
+	
+		
+			System.out.print(a[1]);
+		
+		
+	}
+}
