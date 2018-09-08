712dd2e00d91a49cf3a5e7da4bbeb5ac7a5f05d2
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-08 16:01:45 +0530

    Create removekele.java

---
+++ akhila1/removekele.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[20];
+		n=sc.nextInt();
+		int m=0;
+		int k=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<(n-k);i++)
+		{       if(m<1)
+		         {
+			System.out.print(a[i]);
+		         }
+		         else
+		         {
+		         	System.out.print(" ");	
+		         	System.out.print(a[i]);
+		         }
+		         m++;
+		}
+	}
+}
