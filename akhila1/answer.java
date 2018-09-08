e33a02d2ed7357fa1b7dede651139d6d36685341
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-08 16:08:06 +0530

    Create answer.java

---
+++ akhila1/answer.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String str1=" Answer";
+		String str3=str+str1;
+		System.out.print(str3);
+		
+		
+	}
+}
