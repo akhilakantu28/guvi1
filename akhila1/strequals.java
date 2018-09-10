c0575a32c9f7a65f403af5015816f8db0a588845
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-10 16:41:07 +0530

    Create strequals.java

---
+++ akhila1/strequals.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str,str1;
+		str=sc.next();
+		str1=sc.next();
+		if(str.equals(str1))
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+				System.out.print("no");
+		}
+	}
+}
