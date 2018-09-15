dcc76e5a3a3ddbe7335256755d70873850b83e7d
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-15 13:06:57 +0530

    Create bintodec.java

---
+++ akhila1/bintodec.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 Scanner s=new Scanner(System.in);
+            
+           
+            int n=s.nextInt();
+            
+            int decimal=0,p=0;
+            
+            while(n!=0)
+            {
+                decimal+=((n%10)*Math.pow(2,p));
+                n=n/10;
+                p++;
+            }
+            
+            System.out.println(decimal);
+	}
+}
