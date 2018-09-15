7f775ed76ce7a164ef493a540fb22f3f0fded4bd
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-15 10:23:17 +0530

    Update mergearrays.java

--- akhila1/mergearrays.java
+++ akhila1/mergearrays.java
@@ -13,12 +13,13 @@ class Ideone
 		Scanner sc=new Scanner(System.in);
 		
 		int n=sc.nextInt();
+		int n1=sc.nextInt();
 		int a[]=new int[n];
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
-		int n1=sc.nextInt();
+		
 		int b[]=new int[n1];
 		
 		for(int i=0;i<n1;i++)
