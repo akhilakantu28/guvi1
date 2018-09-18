34fe7623057d7d3846203f7ae4e64d905eaf53c5
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-18 12:33:19 +0530

    Create leftshift.java

---
+++ akhila1/leftshift.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int res=n<<k;
+		System.out.print(res);
+	}
+}
