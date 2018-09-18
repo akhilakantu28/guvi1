9001355b84de3b664d50c1712a7de5c589b4af62
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-18 12:40:21 +0530

    Create bitwisenot.java

---
+++ akhila1/bitwisenot.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	int y=~n;
+		System.out.print(y);
+	}
+}
