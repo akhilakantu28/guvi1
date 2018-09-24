82fcf976b11e978802a7935b790891d55175b36b
akhilakantu28 <41736298+akhilakantu28@users.noreply.github.com>
2018-09-24 14:33:13 +0530

    Create sumofadj.java

---
+++ akhila1/sumofadj.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int sum=0;
+		for(int i=0;i<n-1;i++)
+		{
+			sum=sum+a[i]+a[i+1];
+		}
+		System.out.print(sum);
+	}
+}
